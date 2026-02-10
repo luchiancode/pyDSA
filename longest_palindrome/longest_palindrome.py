ec5970e09791ab051de859c16517f002fe32d3ea
luchiancode <luchian.ionica@outlook.com>
2026-02-10 19:22:42 +0200

    use index

--- longest_palindrome/longest_palindrome.py
+++ longest_palindrome/longest_palindrome.py
@@ -6,7 +6,7 @@ class Solution:
         if(s == s[::-1]): return s
 
         for index in range(0, len(s) - 1):
-            for r_index in range(len(s), -1, -1):
+            for r_index in range(len(s), index, -1):
                 placeholder = s[index:r_index]
                 if(placeholder == placeholder[::-1] and len(placeholder) > len(new_string)): 
                     new_string = placeholder
