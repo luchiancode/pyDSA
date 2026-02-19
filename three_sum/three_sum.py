653def650a6a97e2cd3a6a83a5b348fb58b203c5
luchiancode <luchian.ionica@outlook.com>
2026-02-19 14:04:43 +0200

    remove unnecessary check

--- three_sum/three_sum.py
+++ three_sum/three_sum.py
@@ -4,13 +4,12 @@ class Solution:
 
         for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
-                if(i!=j):
-                    difference = nums[i]+nums[j]
-                    if(-difference in nums):
-                        index = nums.index(-difference)
-                        if(i!=j and i!=index and j!=index):
-                            pair = sorted([nums[i], nums[j], nums[index]])
-                            if(pair not in solution):
-                                solution.append(pair)
+                difference = nums[i]+nums[j]
+                if(-difference in nums):
+                    index = nums.index(-difference)
+                    if(i!=j and i!=index and j!=index):
+                        pair = sorted([nums[i], nums[j], nums[index]])
+                        if(pair not in solution):
+                            solution.append(pair)
         
         return solution
